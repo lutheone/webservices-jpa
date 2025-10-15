034b5546d35a6010166baf845161e4166d73fad5
Luiz Henrique Oliveira <contato3luizhenriqueoliveira@gmail.com>
2025-10-15 17:45:03 -0300

    Component registration and Service layer

--- src/main/java/com/lutheone/webservices/resources/UserResource.java
+++ src/main/java/com/lutheone/webservices/resources/UserResource.java
@@ -1,18 +1,35 @@
 package com.lutheone.webservices.resources;
 
 import com.lutheone.webservices.entities.User;
+import com.lutheone.webservices.services.UserService;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 
+    @Autowired
+    private UserService service;
+
     @GetMapping
-    public ResponseEntity<User> findAll() {
-        User u = new User(1L, "123456", "988888888", "maria@gmail.com", "Maria Brown" );
-        return ResponseEntity.ok().body(u); }
+    public ResponseEntity<List<User>> findAll() {
+        List<User> list = service.findAll();
+        return ResponseEntity.ok().body(list);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<User> findById(@PathVariable Long id) {
+        User obj = service.findById(id);
+        return ResponseEntity.ok().body(obj);
+    }
 }
 
---
+++ src/main/java/com/lutheone/webservices/services/UserService.java
@@ -0,0 +1,28 @@
+package com.lutheone.webservices.services;
+
+import com.lutheone.webservices.entities.User;
+import com.lutheone.webservices.repositories.UserRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository repository;
+
+    public List<User> findAll() {
+        return repository.findAll();
+    }
+
+    public User findById(Long id) {
+        Optional<User> obj = repository.findById(id);
+        return obj.get();
+    }
+}
