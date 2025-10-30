70d8b3432c83d30aaf106d7b9fda4c6469951f17
Luiz Henrique Oliveira <contato3luizhenriqueoliveira@gmail.com>
2025-10-30 19:44:40 -0300

    Exception handling findById

--- src/main/java/com/lutheone/webservices/services/UserService.java
+++ src/main/java/com/lutheone/webservices/services/UserService.java
@@ -3,6 +3,7 @@ package com.lutheone.webservices.services;
 import com.lutheone.webservices.entities.User;
 import com.lutheone.webservices.repositories.UserRepository;
 
+import com.lutheone.webservices.services.exeptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -22,7 +23,7 @@ public class UserService {
 
     public User findById(Long id) {
         Optional<User> obj = repository.findById(id);
-        return obj.get();
+        return obj.orElseThrow(() -> new ResourceNotFoundException(id));
     }
 
     public User insert(User obj) {
