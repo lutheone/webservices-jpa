8d7528f2e94a4b23eec0c6e530b203a43062069f
Luiz Henrique Oliveira <contato3luizhenriqueoliveira@gmail.com>
2025-10-30 20:51:39 -0300

    Exception handling update

--- src/main/java/com/lutheone/webservices/services/UserService.java
+++ src/main/java/com/lutheone/webservices/services/UserService.java
@@ -5,6 +5,7 @@ import com.lutheone.webservices.repositories.UserRepository;
 import com.lutheone.webservices.services.exeptions.DatabaseException;
 import com.lutheone.webservices.services.exeptions.ResourceNotFoundException;
 
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
@@ -45,9 +46,14 @@ public class UserService {
     }
 
     public User update(Long id, User obj) {
-        User entity = repository.getReferenceById(id);
-        updateData(entity, obj);
-        return repository.save(entity);
+        try {
+            User entity = repository.getReferenceById(id);
+            updateData(entity, obj);
+            return repository.save(entity);
+        }
+        catch (EntityNotFoundException e) {
+            throw new ResourceNotFoundException(id);
+        }
     }
 
     private void updateData(User entity, User obj) {
